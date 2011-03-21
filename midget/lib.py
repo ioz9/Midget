9ec383936d955031d34459d96bc0fc88684548ad
Chris Soyars <ctso@ctso.me>
2011-03-20 22:20:42 -0400

    Add copyright notice

--- midget/lib.py
+++ midget/lib.py
@@ -1,3 +1,19 @@
+# This file is part of Midget.
+#
+# Midget is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Midget is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Midget.  If not, see <http://www.gnu.org/licenses>
+#
+# Copyright (c) 2011, Chris Soyars <ctso@ctso.me>
 
 def base36encode(number, alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
     """
--- midget/views.py
+++ midget/views.py
@@ -1,3 +1,20 @@
+# This file is part of Midget.
+#
+# Midget is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Midget is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Midget.  If not, see <http://www.gnu.org/licenses>
+#
+# Copyright (c) 2011, Chris Soyars <ctso@ctso.me>
+
 from pyramid.response import Response
 from pyramid.httpexceptions import HTTPFound
 from midget.models import ShortURL, DBSession
